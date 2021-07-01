b87721bf322e80de90996a0f035ebbcd400cf21d
vitalii <v.budniak@apiko.com>
2021-07-02 02:36:53 +0300

    save

--- src/components/Gallery/BathroomsAndShowers.js
+++ src/components/Gallery/BathroomsAndShowers.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ImageGallery from 'react-image-gallery';
 import './index.css';
+import { useHistory } from "react-router-dom";
 
 export const BathroomsAndShowers = () => {
     const images = [
@@ -16,12 +17,24 @@ export const BathroomsAndShowers = () => {
             original: 'https://picsum.photos/id/1019/1000/600/',
             thumbnail: 'https://picsum.photos/id/1019/250/150/',
         },
+        {
+            original: process.env.PUBLIC_URL + '/assets/bg777.jpg',
+            thumbnail: process.env.PUBLIC_URL + '/assets/bg777.jpg',
+        },
     ];
+
+    let history = useHistory();
+    const back = () => {
+        history.goBack()
+    };
 
     return (
         <div>
             Bathrooms And Showers
-            <ImageGallery items={images} />;
+            <div onClick={back}>
+               Back
+            </div>
+            <ImageGallery items={images} thumbnailPosition={'bottom'} showBullets={true} />
         </div>
     );
 };
