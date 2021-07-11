19e6eb75a2c3ff3edfed751e6eed4051dbb1cbf7
vitalii <v.budniak@apiko.com>
2021-07-11 16:31:13 +0300

    save

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -20,26 +20,20 @@ export const Home = () => {
     const classes = useStyles();
 
     const StyledD = Styled.div`
-            @media screen and (max-width: 768px), 
+            @media screen and (max-width: 768px),
                    screen and (max-height: 630px) {
                           padding-top: 70%;
                     }
-             @media screen and (max-width: 768px), 
-                    screen and (max-height: 665px) {
-                          padding-top: 60%;
+           @media screen and (max-width: 414px),
+                   screen and (max-height: 736px) {
+                          padding-top: 40%;
                     }
-             @media screen and (max-width: 768px), 
-                    screen and (max-height: 670px) {
-                          padding-top: 50%;
+             @media only screen and (device-width: 768px)  {
+                          margin-top: -25%;
                     }
-            @media screen and (max-width: 1024px), 
-                    screen and (max-height: 1024px) {
-                            margin-top: -35%;
-                            align-content: space-between;
-                          
-                           
-               
-            }              
+              @media only screen and (device-width: 1024px) {
+                          margin-top: -35%;
+                    }
              
     `;
 
@@ -55,3 +49,22 @@ export const Home = () => {
         </Fragment>
     )
 };
+
+// @media screen and (max-width: 1024px),
+// screen and (max-height: 1366px) {
+//     margin-top: -35%;
+//     align-content: space-between;
+//
+//
+//
+// }
+
+
+// @media screen and (max-width: 768px),
+// screen and (max-height: 630px) {
+//     padding-top: 70%;
+// }
+// @media screen and (max-width: 768px),
+// screen and (max-height: 665px) {
+//     padding-top: 60%;
+// }
