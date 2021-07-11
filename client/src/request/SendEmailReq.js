f266a4179ddbe56c2a0f9a8ec71aedac43052850
vitalii <v.budniak@apiko.com>
2021-07-12 00:09:16 +0300

    fix emil

--- client/src/request/SendEmailReq.js
+++ client/src/request/SendEmailReq.js
@@ -1,3 +1,3 @@
 import { request } from "./request";
 
-export const emailSend  = async (form) => request(`/api/mail-send`, 'POST', { form });
+export const emailSend  = async (form) => request(`http://80.249.147.193:5000/api/mail-send`, 'POST', { form });
